84f684d2538364b838168a375aede0357fd85810
Gaoutaman <g.man.9931@gmail.com>
2024-01-15 18:28:48 +0000

    Initial commiit

---
+++ Finance_Calculator.py
@@ -0,0 +1,17 @@
+"""
+ - Interest rate calculator
+    -Compound, simple, continuous interest; allow user to input principle amount, interest rate, time period.
+    -Future value of savings
+- Loan calculator
+    Calculates monthly payment, total payment.
+    User inputs loan amount, interest rate, loan duration
+    Amortisation - split payment into principle and interest
+- Allow user to convert interest rates for comparison
+    - Nominal vs Real (inflation taken into account)
+    - Different times
+- File handling
+    - Create reports and summaries
+    - Create databases
+    - Create graphs and visualisations
+- Error handling
+"""
