282909d91a75249ff24e861c9c2179403b56eadf
Gaoutaman <g.man.9931@gmail.com>
2024-01-15 19:23:41 +0000

    menu implementation

--- Finance_Calculator.py
+++ Finance_Calculator.py
@@ -14,4 +14,49 @@
     - Create databases
     - Create graphs and visualisations
 - Error handling
+- GUI? Tkinter
 """
+
+
+# Savings calculator
+def calculate_balance(interest_type, principle, interest, time):
+    if interest_type == "1":
+        balance = principle * (1 + interest * time)
+    elif interest_type == "2":
+        pass
+    else:
+        return
+
+
+while True:
+    print("Welcome!")
+    print(
+        """
+        1. Savings
+        2. Loans
+        3. Interest Rate Converter
+        4. Exit\n"""
+    )
+
+    choice = input("Please choose an option: ")
+
+    if choice == "1":
+        print("Savings")
+        interest_type = input(
+            """
+            1. Simple
+            2. Compound\n"""
+        )
+        principle = float(input("Enter principle value: "))
+        interest = float(input("Enter interest rate: "))
+        time = float(input("Enter term (years): "))
+        balance = calculate_balance(interest_type, principle, interest, time)
+    elif choice == "2":
+        break
+    elif choice == "3":
+        break
+    elif choice == "4":
+        print("Exiting application...")
+        exit()
+    else:
+        print("Invalid choice...\n")
